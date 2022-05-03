b2f61fa9d70f875c2f93e3dd2a1bbbb6698208d0
Glenn Naftali <92601081+glennaftali@users.noreply.github.com>
2022-05-03 20:48:54 +0700

    dev

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ import {AiFillFolderOpen} from 'react-icons/ai'
 
 const About = () => {
   return <section id="about">
-    <h5>Get to know</h5>
+    <h5>Get to know me</h5>
     <h2>About Me</h2>
 
     <div className="container about__container">
